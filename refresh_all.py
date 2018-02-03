365226602e367d4f1d73d32e164567a3cc0e8a30
Benjamin Chen <s910190benjamin@gmail.com>
2018-02-03 20:37:42 +0800

    init

---
+++ refresh_all.py
@@ -0,0 +1,363 @@
+
+# coding: utf-8
+
+import datetime
+import MySQLdb as mysql
+import queue, time, threading
+import sys
+import requests
+import json as jsonpkg
+from bs4 import BeautifulSoup as bs
+
+def initer():
+	initStartTime = datetime.datetime.now()
+	print ('[Init] Start!')
+	while True:
+		try:
+			res = requests.get("http://course-query.acad.ncku.edu.tw/qry/", timeout = 30)
+			while (res.status_code != 200):
+				print ('[ERR] Unexpected error code while requests, Job(INIT) Code : {0}!'.format(res.status_code))
+		except requests.Timeout as e:
+			print ('[Init] timeout!')# str(datetime.datetime.now() - st)
+			continue
+		except (requests.ConnectionError, ConnectionResetError) as e:
+			print ("[Init] " + self.name + " error")# :" + str(datetime.datetime.now() - st)
+			continue
+		except:
+			print ("\n!!! Unexpected error while requests !!!\n")
+			raise
+		else:
+			break
+	res.close()
+	res.encoding = "utf-8"
+	if res.status_code == 200:
+		print ('[Init] Get Main Page succeed!')
+	btfs = bs(res.text, "html.parser")
+	jsonLink = {}
+	for classes in btfs.select('div .dept'):
+		for a in classes.find_all('a', href = True):
+			textTmp = a.text
+			jsonLink[textTmp[3:5]] = {
+				'url':a['href'], 'name':textTmp.replace(u"）", ")").replace(" ","")
+			}
+	for classes in btfs.select('div .institute'):
+		for a in classes.find_all('a', href = True):
+			textTmp = a.text
+			jsonLink[textTmp[3:5]] = {
+				'url':a['href'], 'name':textTmp.replace(u"）", ")").replace(" ","")
+			}
+	print ('[Init] {0} depts! Spending Time = {1}'.format(len(jsonLink), datetime.datetime.now()-initStartTime))
+	return jsonLink
+
+class Job:
+	def __init__(self, name, dept, crawlerCtr):
+		self.name = name
+		self.dept = dept
+		self.crawlerCtr = crawlerCtr
+	def do(self, thisThdName):
+		while True:
+			st = datetime.datetime.now()
+			try:
+				resForClass = requests.get("http://course-query.acad.ncku.edu.tw/qry/" + self.dept['url'], timeout = 30)
+				while (resForClass.status_code != 200):
+					print ('[ERR] Unexpected error code while requests, Job({0}) Code : {1}!'.format(self.name, resForClass.status_code))
+			except requests.Timeout as e:
+				print ('[Crawler] {0} timeout on {1}!'.format(self.name, thisThdName))# str(datetime.datetime.now() - st)
+				continue
+			except (requests.ConnectionError, ConnectionResetError) as e:
+				print ("[Crawler] " + self.name + " error")# :" + str(datetime.datetime.now() - st)
+				continue
+			except:
+				print ("\n!!! Unexpected error while requests !!!\n")
+				raise
+			else:
+				resForClass.encoding = "utf-8"
+				btfsClass = bs(resForClass.text, "html.parser")
+				# 抽取課程內容
+				bodys = []
+				bodys.append([])
+				colctr = 0
+				rowctr = 0
+				for body in btfsClass.find_all('td'):
+					if not int(colctr/len(heads)) == rowctr: # 確認換列 colctr/len(heads) 為應該所在之行數
+						bodys.append([])
+						rowctr += 1
+					bodys[rowctr].append(body.text) # 一格一格填
+					colctr += 1
+				# 輸出至JSON
+				lastKeyTmp ={}
+				for classindex, classinfo in enumerate(bodys):
+					keyTmp = {}
+					for index, data in enumerate(classinfo):
+						if data == u'查無課程資訊': break
+						if heads[index] == 'dept_name':
+							keyTmp[heads[index]] = data.strip().replace(' ', '')
+						else:
+							keyTmp[heads[index]] = data.strip()
+					if 'serial' in keyTmp:
+						keyTmp['course_name'].replace(',','、')
+						if keyTmp['dept_name'] == "":
+							keyTmp['dept_name'] = lastKeyTmp['dept_name']
+						if keyTmp['dept_code'] == "":
+							keyTmp['dept_code'] = lastKeyTmp['dept_code']
+						if keyTmp['credit'] == "":
+							keyTmp['credit'] = '0'
+						try:
+							int(keyTmp['extra_amount'])
+						except ValueError as e:
+							keyTmp['extra_amount'] = '0'
+						if not keyTmp['serial'] == "":
+							keyTmp['serial'] = keyTmp['dept_code'] + keyTmp['serial']
+					if not keyTmp == {}:
+						lastKeyTmp = keyTmp
+						classARR.append(keyTmp)
+				td = datetime.datetime.now() - st
+				print ('[Crawler #{3}] Job({0}) is done by {2}! Spending time = {1}!'.format(self.name, td, thisThdName, self.crawlerCtr))
+				break
+
+def doJob(*args):
+	queue = args[0]
+	thdName = args[1]
+	waitingID = args[2]
+	while True:
+		if queue.qsize() == 0:
+			time.sleep(1)
+			# print ('[Init] {0} waiting in que'.format(thdName))
+			waiting[waitingID] = True
+		if queue.qsize() > 0:
+			job = que.get()
+			job.do(thdName)
+			waiting[waitingID] = False
+
+heads = ['dept_name', 'dept_code', 'serial', 'course_code', 'class_code', 'class_type',
+'grade', 'type', 'group', 'english', 'course_name', 'subject_type', 'credit', 'teacher', 
+'choosed_amount', 'extra_amount', 'time', 'classroom', 'description', 'condition',
+'expert', 'attribute_code', 'cross_master', 'Moocs']
+
+db_config = {}
+
+with open( 'config.json') as f:
+	db_config = jsonpkg.load(f)
+
+db_config['charset'] = 'utf8'
+cnx = mysql.connect(**db_config)
+cursor = cnx.cursor(mysql.cursors.DictCursor)
+que = queue.Queue()
+classARR = []
+waiting = []
+circleStartTime = 0
+# for i in range(4):
+for i in range(2):
+	waiting.append(False)
+
+# fileLocation = {}
+# with open('config.json') as f:
+# 	fileLocation = jsonpkg.load(f)
+# fileLocation = fileLocation['savingLocation']
+
+def queUpdater():
+	global circleStartTime
+	global queStartTime
+	while True:
+		if que.qsize() == 0:
+			if circleStartTime == 0:
+				circleStartTime = datetime.datetime.now()
+				jsonLink = initer()
+				crawlerCtr = 1
+				queStartTime = datetime.datetime.now()
+				for num, dept in enumerate(jsonLink):
+					# if num >= 4: break
+					# print (dept)
+					que.put(Job(dept, jsonLink[dept], crawlerCtr))
+					crawlerCtr += 1
+				print ("[Queueing] Queue size = {0}...!".format(que.qsize()))
+			elif not False in waiting:
+				print ('[Queueing] Done que! Spending time = {0}!'.format(datetime.datetime.now()-queStartTime))
+				# print (classARR)
+				dbUpdater()
+				print ('[ALL] Done all! Spending time = {0}!'.format(datetime.datetime.now()-circleStartTime))
+				circleStartTime = datetime.datetime.now()
+				jsonLink = initer()
+				crawlerCtr = 1
+				queStartTime = datetime.datetime.now()
+				for num, dept in enumerate(jsonLink):
+					# if num >= 4: break
+					# print dept
+					que.put(Job(dept, jsonLink[dept], crawlerCtr))
+					crawlerCtr += 1
+				print ("[Queueing] Queue size = {0}...!".format(que.qsize()))
+		time.sleep(5)
+
+def dbUpdater():
+	global classARR
+	localClassARR = list(classARR)
+	classARR = []
+	idList = []
+	followedList = []
+	tmpOriCourses = {}
+	gotErr = False
+	logOutput = ""
+
+	print ('[Selecter] Start Select! Select Size = {0}'.format(len(localClassARR)))
+	startTime = datetime.datetime.utcnow()
+	errCtr = 0
+	while True:
+		if errCtr < 5:
+			try:
+				cursor.execute(""" SELECT * FROM course_new; """)
+			except Exception as e:
+				print ("error on exec SELECT ori_course : {0}".format(e))
+				errCtr += 1
+				continue
+			else:
+				for row in cursor:
+					tmpKey = row['系號'] + '-' + row['課程碼'] + '-' + row['分班碼'] + '-' + row['組別'] + '-' + row['類別'] + '-' + row['班別']
+					tmpOriCourses[tmpKey] = row['id']
+				break
+		else:
+			raise Exception("Retry Too Many Times")
+	for aCourse in localClassARR:
+		tmpKey = aCourse['dept_code'] + '-' + aCourse['course_code'] + '-' + aCourse['class_code'] + '-' + aCourse['group'] + '-' + aCourse['type'] + '-' + aCourse['class_type']
+		aCourse['id'] = tmpOriCourses.get(tmpKey, -1)
+		if aCourse['id'] == -1:
+			logOutput += '[NEW] |' + datetime.datetime.today().isoformat() + '| ' + tmpKey + '\n'
+	print ('[Selecter] Finish Select! Spending time = {0}!'.format(datetime.datetime.utcnow()-startTime))
+
+	print ('[Selecter] Start Getting Followed List!')
+	startTime = datetime.datetime.utcnow()
+	errCtr = 0
+	while True:
+		if errCtr < 5:
+			try:
+				cursor.execute("SELECT * FROM follow;")
+			except Exception as e:
+				print ("error on exec SELECT follow list : {0}".format(e))
+				errCtr += 1
+				continue
+			else:
+				for row in cursor:
+					if row['hadNotify'] == 0 and not row['course_id'] == -1:
+						followedList.append(row['serial'])
+				break
+		else:
+			raise Exception("Retry Too Many Times")
+	print ('[Selecter] Finish Select! Spending time = {0}!'.format(datetime.datetime.utcnow()-startTime))
+
+	print ('[Updater] Start Update and Insert!')
+	timeStamp = datetime.datetime.utcnow()
+	for aCourse in localClassARR:
+		if not aCourse['id'] == -1:
+			if aCourse['serial'] in followedList:
+				try:
+					cursor.execute("""
+					UPDATE course_new
+					SET `英語授課`=%s, `學分`=%s, `老師`=%s, `已選課人數`=%s, `時間`=%s, `教室`=%s, `備註`=%s, `限選條件`=%s, 
+					`業界參與`=%s, `屬性碼`=%s, `跨領域學分學程`=%s, `Moocs`=%s, `updateTime`=%s WHERE `id`=%s;
+					""", (aCourse['english'], float(aCourse['credit']), aCourse['teacher'], int(aCourse['choosed_amount']),
+					aCourse['time'], aCourse['classroom'], aCourse['description'], aCourse['condition'], aCourse['expert'],
+					aCourse['attribute_code'], aCourse['cross_master'], aCourse['Moocs'], datetime.datetime.utcnow(), aCourse['id']))
+				except Exception as e:
+					print ("error on exec UPDATE [ {0} ] : {1}".format(aCourse, e))
+					gotErr = True
+			else:
+				try:
+					cursor.execute("""
+					UPDATE course_new
+					SET `英語授課`=%s, `學分`=%s, `老師`=%s, `已選課人數`=%s, `餘額`=%s, `時間`=%s, `教室`=%s, 
+					`備註`=%s, `限選條件`=%s, `業界參與`=%s, `屬性碼`=%s, `跨領域學分學程`=%s, `Moocs`=%s, `updateTime`=%s
+					WHERE `id`=%s;
+					""", (aCourse['english'], float(aCourse['credit']), aCourse['teacher'], int(aCourse['choosed_amount']),
+					aCourse['extra_amount'], aCourse['time'], aCourse['classroom'], aCourse['description'],
+					aCourse['condition'], aCourse['expert'], aCourse['attribute_code'], aCourse['cross_master'],
+					aCourse['Moocs'], datetime.datetime.utcnow(), aCourse['id']))
+				except Exception as e:
+					print ("error on exec UPDATE [ {0} ] : {1}".format(aCourse, e))
+					gotErr = True
+		else:
+			try:
+				cursor.execute("""
+				INSERT INTO course_new (`系所名稱`,`系號`,`選課序號`,`課程碼`,`分班碼`,`班別`,`年級`,`類別`,`組別`,`英語授課`,`課程名稱`,
+				`選必修`,`學分`,`老師`,`已選課人數`,`餘額`,`時間`,`教室`,`備註`,`限選條件`,`業界參與`,`屬性碼`,`跨領域學分學程`,`Moocs`,`updateTime`)
+				VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+				""", (aCourse['dept_name'], aCourse['dept_code'], aCourse['serial'], aCourse['course_code'], aCourse['class_code'],
+				aCourse['class_type'], aCourse['grade'], aCourse['type'], aCourse['group'], aCourse['english'], aCourse['course_name'],
+				aCourse['subject_type'], float(aCourse['credit']), aCourse['teacher'], int(aCourse['choosed_amount']), aCourse['extra_amount'],
+				aCourse['time'], aCourse['classroom'], aCourse['description'], aCourse['condition'], aCourse['expert'],
+				aCourse['attribute_code'], aCourse['cross_master'], aCourse['Moocs'], datetime.datetime.utcnow()))
+			except Exception as e:
+				print ("error on exec INSERT [ {0} ] : {1}".format(aCourse, e))
+	cnx.commit()
+	print ('[Updater] Finish Update and Insert! Spending time = {0}!'.format(datetime.datetime.utcnow()-timeStamp))
+	
+	if not gotErr:
+		print ('[Select] Start Select non-update!')
+		startTime = datetime.datetime.now()
+		try:
+			query = "SELECT * FROM course_new WHERE `updateTime` < '{0}';".format(timeStamp)
+			cursor.execute(query)
+		except Exception as e:
+			print ("error on exec SELECT non-update : {0}".format(e))
+		else:
+			for row in cursor:
+				idList.append(row['id'])
+				tmpKey = str(row['id']) + '-' + row['系號'] + '-' + row['課程碼'] + '-' + row['分班碼'] + '-' + row['組別'] + '-' + row['類別'] + '-' + row['班別']
+				logOutput += '[OLD] |' + datetime.datetime.today().isoformat() + '| '  + tmpKey + '\n'
+		print ('[Select] Finish Select non-update! Spending time = {0}!'.format(datetime.datetime.now()-startTime))
+
+		# print ('[Delete] Start Clean non-update! Amount: {0}'.format(len(idList)))
+		# startTime = datetime.datetime.now()
+		# for aCourse in idList:
+		# 	try:
+		# 		query = "DELETE FROM course_new WHERE id = {0};".format(aCourse)
+		# 		cursor.execute(query)
+		# 	except Exception as e:
+		# 		print ("error on exec DELETE non-update [ {0} ] : {1}".format(aCourse, e))
+		# 	else:
+		# 		print ("[Delete] Course Close [ {0} ]".format(aCourse))
+		# cnx.commit()
+		# print ('[Delete] Finish Clean non-update! Spending time = {0}!'.format(datetime.datetime.now()-startTime))
+
+		# print ('[ModFollow] Start Modify Follow! Amount: {0}'.format(len(idList)))
+		# startTime = datetime.datetime.now()
+		# for aCourse in idList:
+		# 	try:
+		# 		query = "UPDATE follow SET course_id=-1 WHERE course_id={0};".format(aCourse)
+		# 		cursor.execute(query)
+		# 	except Exception as e:
+		# 		print ("error on exec UPDATE closedCourse Follow [ {0} ] : {1}".format(aCourse, e))
+		# cnx.commit()
+		# print ('[ModFollow] Finish Modify Follow! Spending time = {0}!'.format(datetime.datetime.now()-startTime))
+
+	with open('devLog', 'a') as f:
+		f.write(logOutput)
+
+try:
+	thd1 = threading.Thread(target=doJob, name='Thd1', args=(que,'Thd[1]', 0))
+	thd2 = threading.Thread(target=doJob, name='Thd2', args=(que,'Thd[2]', 1))
+	# thd3 = threading.Thread(target=doJob, name='Thd3', args=(que,'Thd[3]', 2))
+	# thd4 = threading.Thread(target=doJob, name='Thd4', args=(que,'Thd[4]', 3))
+	thd5 = threading.Thread(target=queUpdater, name='Thd5', args=())
+	thd1.daemon = True
+	thd2.daemon = True
+	# thd3.daemon = True
+	# thd4.daemon = True
+	thd5.daemon = True
+	crawlStartTime = datetime.datetime.now()
+	thd1.start()
+	thd2.start()
+	# thd3.start()
+	# thd4.start()
+	thd5.start()
+	while True:
+		if not (thd1.is_alive() and thd2.is_alive() and thd5.is_alive()):
+		# if not (thd1.is_alive() and thd2.is_alive() and thd3.is_alive() and thd4.is_alive() and thd5.is_alive()):
+			raise Exception('Thread Dead')
+		time.sleep(10)
+except (KeyboardInterrupt, SystemExit):
+	print ('\n!!! Received keyboard interrupt, quitting threads. !!!\n')
+except:
+	print (sys.exc_info())
+	print ('\n!!! A thread dead, stop all !!!\n')
+else:
+	cursor.close()
+	cnx.close()
---
+++ update_amount.py
@@ -0,0 +1,214 @@
+
+# coding: utf-8
+
+import datetime
+import MySQLdb as mysql
+import queue, time, threading
+import sys
+import requests
+import json as jsonpkg
+from bs4 import BeautifulSoup as bs
+
+class Job:
+	def __init__(self, name, dept):
+		self.name = name
+		self.dept = dept
+	def do(self, thisThdName):
+		while True:
+			st = datetime.datetime.now()
+			try:
+				resForClass = requests.get("http://course-query.acad.ncku.edu.tw/qry/" + self.dept['url'], timeout = 30)
+				while (resForClass.status_code != 200):
+					print ('[ERR] Unexpected error code while requests, Job({0}) Code : {1}!'.format(self.name, resForClass.status_code))
+			except requests.Timeout as e:
+				print ('[Crawler] {0} timeout on {1}!'.format(self.name, thisThdName))# str(datetime.datetime.now() - st)
+				continue
+			except requests.ConnectionError as e:
+				print (self.name + " error")# :" + str(datetime.datetime.now() - st)
+				continue
+			except:
+				print ("Unexpected error while requests")
+				break
+			else:
+				resForClass.encoding = "utf-8"
+				btfsClass = bs(resForClass.text, "html.parser")
+				# 抽取課程內容
+				bodys = []
+				bodys.append([])
+				colctr = 0
+				rowctr = 0
+				for body in btfsClass.find_all('td'):
+					if not int(colctr/len(heads)) == rowctr: # 確認換列 colctr/len(heads) 為應該所在之行數
+						bodys.append([])
+						rowctr += 1
+					bodys[rowctr].append(body.text) # 一格一格填
+					colctr += 1
+				# 輸出至newDataPool
+				for classindex, classinfo in enumerate(bodys):
+					key = ""
+					keyTmp = {}
+					for index, data in enumerate(classinfo):
+						if data == u'查無課程資訊': break
+						if heads[index] == 'dept_name':
+							keyTmp[heads[index]] = data.strip().replace(' ', '')
+						else:
+							keyTmp[heads[index]] = data.strip()
+						if index == 1 or index == 2: 
+							key += data
+						try:
+							int(keyTmp['extra_amount'])
+						except ValueError as e:
+							keyTmp['extra_amount'] = '0'
+					if not key == "":
+						newDataPool[key] = keyTmp
+				td = datetime.datetime.now() - st
+				print("[Crawler] Job({0}) is done by {2}! Spending time = {1}!".format(self.name, td, thisThdName))
+				break
+
+def queUpdater():
+	global dataToUpdate
+	global circleStartTime
+	global queUpdaterWorking
+	cnxTmp = mysql.connect(**db_config)
+	cursorTmp = cnxTmp.cursor(mysql.cursors.DictCursor)
+	query = ("SELECT * FROM follow")
+	cursorTmp.execute(query)
+	followedList = []
+	for row in cursorTmp:
+		if row['hadNotify'] == 0 and not row['course_id'] == -1:
+			followedList.append(row['serial'])
+	cnxTmp.close()
+	cursorTmp.close()
+	deptList = []
+	serialDictByDept = {}
+	for text in followedList:
+		if len(text) == 5:
+			if not text[0:2] in serialDictByDept:
+				serialDictByDept[text[0:2]] = []
+				serialDictByDept[text[0:2]].append(text)
+				deptList.append(text[0:2])
+			else:
+				if not text in serialDictByDept[text[0:2]]:
+					serialDictByDept[text[0:2]].append(text)
+	# print deptList
+	# print serialDictByDept
+	for num, dept in enumerate(deptList):
+		# if num >= 4: break
+		# print dept
+		que.put(Job(dept, deptDict[dept]))
+	print("[Queueing] Queue size = {0}... Circle time = {1}!".format(que.qsize(), datetime.datetime.now()-circleStartTime))
+	dataToUpdate = serialDictByDept
+	circleStartTime = datetime.datetime.now()
+	queUpdaterWorking = False
+
+def doJob(*args):
+	global queUpdaterWorking
+	queueing = args[0]
+	thdName = args[1]
+	while True:
+		if queueing.qsize() == 0:
+			if not queUpdaterWorking:
+				queUpdater()
+			else:
+				time.sleep(1)
+		if queueing.qsize() > 0:
+			job = que.get()
+			job.do(thdName)
+
+def doJobA9():
+	while True:
+		job = Job('A9', deptDict['A9'])
+		job.do('Thd[9]')
+
+res = requests.get("http://course-query.acad.ncku.edu.tw/qry/")
+res.close()
+res.encoding = "utf-8"
+if res.status_code == 200:
+    print ("[Init] Get Main Page Succeed")
+btfs = bs(res.text, "html.parser")
+deptDict = {}
+for classes in btfs.select('div .dept'): # Undergraduate
+    for a in classes.find_all('a', href = True):
+        textTmp = a.text
+        deptDict[textTmp[3:5]] = {
+            'url':a['href'], 'name':textTmp.strip()
+        }
+for classes in btfs.select('div .institute'): # Graduate
+    for a in classes.find_all('a', href = True):
+        textTmp = a.text
+        deptDict[textTmp[3:5]] = {
+            'url':a['href'], 'name':textTmp.strip()
+        }
+print ("[Init] {0} depts".format(len(deptDict)))
+
+heads = ['dept_name', 'dept_code', 'serial', 'course_code', 'class_code', 'class_type',
+'grade', 'type', 'group', 'english', 'course_name', 'subject_type', 'credit', 'teacher', 
+'choosed_amount', 'extra_amount', 'time', 'classroom', 'description', 'condition',
+'expert', 'attribute_code', 'cross_master', 'Moocs']
+
+db_config = {}
+
+with open( 'config.json') as f:
+	db_config = jsonpkg.load(f)
+
+db_config['charset'] = 'utf8'
+cnx = mysql.connect(**db_config)
+cursor = cnx.cursor(mysql.cursors.DictCursor)
+que = queue.Queue()
+dataToUpdate = {}
+newDataPool = {}
+queUpdaterWorking = False
+
+queUpdater()
+circleStartTime = datetime.datetime.now()
+
+try:
+	thd1 = threading.Thread(target=doJob, name='Thd1', args=(que,'Thd[1]'))
+	thd2 = threading.Thread(target=doJob, name='Thd2', args=(que,'Thd[2]'))
+	thd3 = threading.Thread(target=doJob, name='Thd3', args=(que,'Thd[3]'))
+	# thd4 = threading.Thread(target=doJob, name='Thd4', args=(que,'Thd[4]'))
+	thd5 = threading.Thread(target=doJobA9, name='Thd5', args=())
+	thd1.daemon = True
+	thd2.daemon = True
+	thd3.daemon = True
+	# thd4.daemon = True
+	thd5.daemon = True
+	thd1.start()
+	thd2.start()
+	thd3.start()
+	# thd4.start()
+	thd5.start()
+	while True:
+		print ('[Update] Start query to update Extra Amount!')
+		queryStartTime = datetime.datetime.now()
+		tmpNewDataPool = newDataPool
+		newDataPool = {}
+		for aDept in dataToUpdate:
+			for serial in dataToUpdate[aDept]:
+				if serial in tmpNewDataPool:
+					balance = tmpNewDataPool[serial]["extra_amount"]
+					while True:
+						try:
+							cursor.execute ("""
+							 UPDATE course_new
+							 SET `餘額`=%s
+							 WHERE `選課序號`=%s
+							""", (balance, serial))
+						except Exception:
+							print ('Error on exec [ {0} : {1} ]'.format(serial, balance))
+							continue
+						else:
+							cnx.commit()
+							break
+		print ('[Update] Query Finish! Spending time = {0}!'.format(datetime.datetime.now()-queryStartTime))
+		if not (thd1.is_alive() and thd2.is_alive() and thd3.is_alive()):# and thd4.is_alive()):
+			raise Exception('Thread Dead')
+		time.sleep(10)
+except (KeyboardInterrupt, SystemExit):
+	print ('\n!!! Received keyboard interrupt, quitting threads. !!!\n')
+except:
+	print (sys.exc_info())
+	print ('\n!!! A thread dead, stop all !!!\n')
+finally:
+	cursor.close()
+	cnx.close()
